52845d9b130474d2e281acb2bbbc0a73aa859d0b
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2016-03-02 01:10:09 -0300

    Cleaned up code

--- 7seg/SixtySeconds.ino
+++ 7seg/SixtySeconds.ino
@@ -1,13 +1,12 @@
 #define LATCH_PIN   8 // Pin connected to Pin 12 of 74HC595 (Latch)
 #define CLOCK_PIN  12 // Pin connected to Pin 11 of 74HC595 (Clock)
 #define DATA_PIN   11 // Pin connected to Pin 14 of 74HC595 (Data)
-//#define BUZZER_PIN 13 // pin connected to buzzer
 
-#define COUNTDOWN_NUMBER 60 // must be an integer in the range 0..99 (i.e., 2-digit)
+#define COUNTDOWN_NUMBER 60 // must be an integer in the range from 0 to 99
 
 // LED digit patterns, from 0 to 9
 // This is for common cathode displays.
-// For common anode displays, simply switch 1's and 0's.
+// For common anode displays, simply switch 1s and 0s.
 // 1 = LED on, 0 = LED off, in this order in the IC: 15,1,2,3,4,5,6,7
 const byte SEVEN_SEG_DIGITS[10] = {
   B01111110, // = 0
@@ -27,28 +26,15 @@ void setup() {
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
   pinMode(DATA_PIN,  OUTPUT);
-  //Serial.begin(9600);
 }
 
 void loop() {
   byte segs1, segs2, count = COUNTDOWN_NUMBER;
   do {
-    /*
-    Serial.print("count = ");
-    Serial.print(count);
-    Serial.print(" [ ");
-    Serial.print(count / 10);
-    Serial.print(" | ");
-    Serial.print(count % 10);
-    Serial.println(" ] ");
-    */
 
     // retrieve bit segments for each digit
     segs1 = SEVEN_SEG_DIGITS[count / 10];
     segs2 = SEVEN_SEG_DIGITS[count % 10];
-
-    //if (count % 2 == 0)
-    //  segs |= B10000000;
 
     // set LATCH_PIN low to allow data flow
     digitalWrite(LATCH_PIN, LOW);
@@ -60,11 +46,10 @@ void loop() {
     // set LATCH_PIN to high to lock and send data
     digitalWrite(LATCH_PIN, HIGH);
 
-    //tone(BUZZER_PIN, 880, 200);
-    
+    // hold on for 1 second
     delay(1000);
+
   } while (count-- > 0);
-  
   delay(4000);
 }
 
